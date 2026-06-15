dfc760ddb1fd6a876343f9c51d498300a1c45067
khushboo-khatoon <khushboo071006@gmail.com>
2026-06-15 15:24:11 +0530

    chore: remove dead commented-out code in app-providers.tsx

--- components/providers/app-providers.tsx
+++ components/providers/app-providers.tsx
@@ -1,20 +1,3 @@
-// "use client"
-
-// import type { ReactNode } from "react"
-// import { ThemeProvider } from "@/components/theme-provider"
-// import { SessionTimeoutManager } from "@/components/session-timeout-manager"
-// import { UserDataProvider } from "@/hooks/use-user"
-
-// export function AppProviders({ children }: { children: ReactNode }) {
-//   return (
-//     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-//       <UserDataProvider>
-//         <SessionTimeoutManager />
-//         {children}
-//       </UserDataProvider>
-//     </ThemeProvider>
-//   )
-// }
 "use client"
 
 import type { ReactNode } from "react"
